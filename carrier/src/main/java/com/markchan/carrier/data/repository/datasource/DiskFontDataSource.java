c23cd2b26aadf49e9a6c88812081cf19202e4580
Mark Chan <markchan163@163.com>
2017-07-20 18:44:32 +0800

    remove old files on database(renamed to dao)

--- carrier/src/main/java/com/markchan/carrier/data/repository/datasource/DiskFontDataSource.java
+++ carrier/src/main/java/com/markchan/carrier/data/repository/datasource/DiskFontDataSource.java
@@ -1,22 +1,24 @@
 package com.markchan.carrier.data.repository.datasource;
 
-import com.markchan.carrier.data.cache.FontCache;
+import com.markchan.carrier.data.cache.FontEntityCache;
 import com.markchan.carrier.data.entity.FontEntity;
 import com.markchan.carrier.data.exception.FontDeletedException;
+
+import java.util.List;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
-import java.util.List;
 
 /**
  * Created by Mark on 2017/7/16
  */
 public class DiskFontDataSource implements FontDataStore {
 
-    private final FontCache mFontCache;
+    private final FontEntityCache mFontEntityCache;
 
-    public DiskFontDataSource(FontCache fontCache) {
-        mFontCache = fontCache;
+    public DiskFontDataSource(FontEntityCache fontEntityCache) {
+        mFontEntityCache = fontEntityCache;
     }
 
     @Override
@@ -25,7 +27,7 @@ public class DiskFontDataSource implements FontDataStore {
 
             @Override
             public void subscribe(ObservableEmitter<FontEntity> e) throws Exception {
-                FontEntity fontEntity = mFontCache.getDownloadedFontEntity(fontId);
+                FontEntity fontEntity = mFontEntityCache.getDownloadedFontEntity(fontId);
                 if (fontEntity != null) {
                     e.onNext(fontEntity);
                     e.onComplete();
@@ -42,7 +44,7 @@ public class DiskFontDataSource implements FontDataStore {
 
             @Override
             public void subscribe(ObservableEmitter<List<FontEntity>> e) throws Exception {
-                List<FontEntity> fontEntities = mFontCache.getDownloadedFontEntities();
+                List<FontEntity> fontEntities = mFontEntityCache.getDownloadedFontEntities();
                 e.onNext(fontEntities);
                 e.onComplete();
             }
